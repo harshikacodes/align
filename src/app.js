fb78c5ac53d56ee2fea4d8cafe7303ec38d502c1
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-18 23:53:54 +0530

    add express server with user endpoint

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({firstName: "Harshika", lastname: "Malhotra"});
+});
+
+app.listen(7777, () => {
+    console.log("Server is sucessfully listening on port 7777...");
+})
