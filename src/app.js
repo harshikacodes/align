48ffcd5998baff2d3c8978b2be97c4f24b9198f3
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-20 15:15:42 +0530

    connect app with database

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
 
-const {adminAuth} = require("./middlewares/auth");
+connectDB()
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(7777, () => {
+            console.log("Server is sucessfully listening on port 7777...");
+        })
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected!!");
+    });
 
-app.use("/admin", adminAuth);
-
-app.get("/user", (req, res) => {
-    res.send("User Data Sent");
-})
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("Deleted a user");
-});
-
-app.listen(7777, () => {
-    console.log("Server is sucessfully listening on port 7777...");
-})
