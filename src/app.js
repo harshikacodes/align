c0a2ba44da844b028b698e3783b8cc1f2e96b364
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-20 16:12:52 +0530

    add user model and signup endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+    // creating a new instance of the User model
+    const user = new User({
+        firstName: "Harshika",
+        lastName: "Malhotra",
+        emailId: "harshika@gmail.com",
+        password: "harshika@1234",
+    })
+
+    try{
+        await user.save();
+        res.send("User added successfully!");
+    } catch (err) {
+        res.status(400).send("Error in saving the user: " + err.message);
+    }
+});
 
 connectDB()
     .then(() => {
