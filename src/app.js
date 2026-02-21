ec09b9027d021b30e8a7f3c621d34d806ec65191
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-21 17:39:00 +0530

    add user retrieval and feed APIs

--- src/app.js
+++ src/app.js
@@ -17,6 +17,35 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+// get user by email id
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId: userEmail});
+
+        if(users.length === 0){
+            res.status(404).send("User Not Found!");
+        } else {
+            res.send(users);
+        }
+        
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+    
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(404).send("Something went wrong")
+    }
+});
+
 connectDB()
     .then(() => {
         console.log("Database connection established...");
