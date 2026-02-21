0cd86c73daaafba153eea7be216a58583114855a
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-21 23:24:53 +0530

    refactor user schema to include required fields and default values

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,42 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
     lastName: {
-        type: String
+        type: String,
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png"
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user!"
+    },
+    skills: {
+        type: [String],
     }
 });
 
