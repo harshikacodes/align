159ece78bb2911f58d63c06bb56de38ae8e3edb2
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-07-24 21:49:58 +0530

    add user data validation during signup process

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    // creating a new instance of the User model
-    const user = new User(req.body);
-
     try {
+        // validation of data
+        validateSignUpData(req);
+        // Encrypt the password
+
+        // store the user into DB
+        // creating a new instance of the User model
+        const user = new User(req.body);
+
         await user.save();
         res.send("User added successfully!");
     } catch (err) {
